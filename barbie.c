4c0128d9d2658ea9fa010afd4cd7be75656459b4
Abhishek Kumar <abhishekkumarjha231@gmail.com>
2025-05-06 02:22:37 +0530

    Update

--- barbie.c
+++ barbie.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<curl/curl.h>
-#include<cjson/cJSON.h>
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// #include<curl/curl.h>
+// #include<cjson/cJSON.h>
 
-// Replace with your OpenWeatherMap API key
-#define API_KEY "your_api_key"
-#define API_URL "your URL"
+// // Replace with your OpenWeatherMap API key
+// #define API_KEY "your_api_key"
+// #define API_URL "your URL"
 
-//FUnction to handle the HTTP response 
-size-t write_callback(void*ptr, size_t size, size_t nmemb, char*data) {
-  strcat(data, ptr);
-  return size * nmemb;
-}
+// //FUnction to handle the HTTP response 
+// size-t write_callback(void*ptr, size_t size, size_t nmemb, char*data) {
+//   strcat(data, ptr);
+//   return size * nmemb;
+// }
 
-//Function to get weather Data
-void get_weather(const char*city) {
-  CURL *curl;
-  CURLcode res;
-  char url[256];
-  char data[2048] = ""; // Store the HTTP response here
+// //Function to get weather Data
+// void get_weather(const char*city) {
+//   CURL *curl;
+//   CURLcode res;
+//   char url[256];
+//   char data[2048] = ""; // Store the HTTP response here
 
-// Prepare the API URL
-snprintf(url, sizeof(url), API_URL, city, API_KEY);
+// // Prepare the API URL
+// snprintf(url, sizeof(url), API_URL, city, API_KEY);
 
-// Initialize libcurl
-curl_global_init(CURL_GLOBAL_DEFAULT);
-curl = curl_easy-init();
+// // Initialize libcurl
+// curl_global_init(CURL_GLOBAL_DEFAULT);
+// curl = curl_easy-init();
 
-if(curl) {
-  // set the URL to fetch
-  curl_easy_setopt(curl, CURLOPT_URL, url);
+// if(curl) {
+//   // set the URL to fetch
+//   curl_easy_setopt(curl, CURLOPT_URL, url);
 
-// Specify the callback Function to write the response data
-    curl_easy_setopt(curl, CURLOPT_URL, url);
+// // Specify the callback Function to write the response data
+//     curl_easy_setopt(curl, CURLOPT_URL, url);
 
