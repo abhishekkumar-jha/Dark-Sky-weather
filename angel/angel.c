9bb59c4fbecb1396b423bd2b41512fdd91ab0341
Angel <angelsidana03@gmail.com>
2025-05-06 18:05:34 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -111,3 +111,14 @@ int main() {
     printf("Swapped: a = %d, b = %d\n", a, b);
     return 0;
 }
+
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}, sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+    printf("Sum: %d\n", sum);
+    return 0;
+}
+
