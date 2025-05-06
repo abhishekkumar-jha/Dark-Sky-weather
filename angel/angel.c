6f34a4b7a991125e76842cccdb9a000f14b0273e
Angel <angelsidana03@gmail.com>
2025-05-06 18:14:35 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -342,3 +342,11 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[] = "Hello, ", str2[] = "World!";
+    strcat(str1, str2);
+    printf("Concatenated String: %s\n", str1);
+    return 0;
+}
