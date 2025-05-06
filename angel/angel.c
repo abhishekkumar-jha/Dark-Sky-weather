37be84ebf1b49ce7f4839c18384e17000094ebe8
Angel <angelsidana03@gmail.com>
2025-05-06 18:01:27 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -52,3 +52,25 @@ int main() {
     printf("Factorial: %d\n", fact);
     return 0;
 }
+
+#include <stdio.h>
+int main() {
+    int n = 10, t1 = 0, t2 = 1, nextTerm;
+    printf("Fibonacci Series: ");
+    for (int i = 1; i <= n; ++i) {
+        if (i == 1) {
+            printf("%d, ", t1);
+            continue;
+        }
+        if (i == 2) {
+            printf("%d, ", t2);
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        printf("%d, ", nextTerm);
+    }
+    return 0;
+}
+
