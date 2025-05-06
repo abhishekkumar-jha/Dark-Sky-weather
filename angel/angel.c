9e06c8f3ecb29f120037fca46f4dc25398f57520
Angel <angelsidana03@gmail.com>
2025-05-06 18:10:21 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -208,3 +208,14 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+int main() {
+    int n = 12345, sum = 0;
+    while (n != 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    printf("Sum of digits: %d\n", sum);
+    return 0;
+}
+
