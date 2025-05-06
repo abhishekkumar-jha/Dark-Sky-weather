aae96daee74ccf7be37d463737c91e29b84bf33e
Angel <angelsidana03@gmail.com>
2025-05-06 18:09:00 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -170,3 +170,14 @@ int main() {
     }
     return 0;
 }
+
+#include <stdio.h>
+int main() {
+    int year = 2024;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("Leap Year\n");
+    } else {
+        printf("Not a Leap Year\n");
+    }
+    return 0;
+}
