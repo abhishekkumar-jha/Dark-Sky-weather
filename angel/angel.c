c51b0b61d11755c0ecb7ebbea43951e5f5416848
Angel <angelsidana03@gmail.com>
2025-05-06 18:13:06 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -308,4 +308,12 @@ int main() {
     return 0;
 }
 
-
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}, sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+    printf("Sum: %d\n", sum);
+    return 0;
+}
