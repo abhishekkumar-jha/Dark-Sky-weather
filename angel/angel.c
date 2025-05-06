d210527cbbb7307ca55c2053e69c833df336afa9
Angel <angelsidana03@gmail.com>
2025-05-06 18:07:02 +0530

    Update angel.c

--- angel/angel.c
+++ angel/angel.c
@@ -122,3 +122,20 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}, temp;
+    int n = 5;
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+
